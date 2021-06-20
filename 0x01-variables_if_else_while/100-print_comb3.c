5bdd00ed4541adde5d6daebe15e0380acf35c051
amikable0179 <amarachukwu.ogbu1993@gmail.com>
2021-06-20 18:30:18 +0000

    this program prints numbersof double digits with commas and space

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,26 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
- *main - Entry point
- *
- *Description: Program will print numbers from 00 to 99 in ascending order
- *as a comma separated list using NO char data types.
- *Return: 0 when successfully run to completion
+ * main - main block
+ * Description: Write a program that prints all possible
+ * different combinations of two digits
+ * Numbers must be separated by ,, followed by a space
+ * The two digits must be different
+ * 01 and 10 are considered the same combination of the two digits 0 and 1
+ * Print only the smallest combination of two digits
+ * Numbers should be printed in ascending order, with two digits
+ * You can only use the putchar function
+ * (every other function (printf, puts, etc…) is forbidden)
+ * You can only use putchar five times maximum in your code
+ * You are not allowed to use any variable of type char
+ * All your code should be in the main function
+ * Return: 0
  */
 int main(void)
 {
-	int num = 0;
-
-	while (num < 100)
-	{
-		putchar((num / 10) + '0');
-		putchar((num % 10) + '0');
-		if (num != 99)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-		num++;
-	}
-	putchar('\n');
-	return (0);
+int c;
+int d = 0;
+while (d < 10)
+{
+c = 0;
+while (c < 10)
+{
+if (d != c && d < c)
+{
+putchar('0' + d);
+putchar('0' + c);
+if (c + d != 17)
+{
+putchar(',');
+putchar(' ');
+}
+}
+c++;
+}
+d++;
+}
+putchar('\n');
+return (0);
 }
