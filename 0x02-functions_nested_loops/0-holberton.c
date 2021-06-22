cfb0db44624c4ad4e9c788832d5251e2945bfe36
amikable0179 <amarachukwu.ogbu1993@gmail.com>
2021-06-22 07:08:20 -0700

    corrected the holberton .h

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <holberton.h>
 /**
 * main - program prints Holberton
@@ -7,7 +6,15 @@
 */
 int main(void)
 {
-	_putchar("Holberton\n");
-
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
 	return (0);
 }
