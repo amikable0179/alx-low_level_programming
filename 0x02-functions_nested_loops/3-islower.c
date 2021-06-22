dfdacee25061a8cdfa74ed87f29df4e4f146dc2c
amikable0179 <amarachukwu.ogbu1993@gmail.com>
2021-06-22 19:29:18 +0000

    program checks for lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+* int _islower - checks for lower case
+*
+* result: 0 if false 1 if true
+*/
+
+int _islower(int c)
+{
+	if(c = 'a' && c <= 'z')
+	return (1);
+
+	else
+	return (0);
+}
