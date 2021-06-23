423efa1b338c77877dc5a385e1b9b93585d2a290
amikable0179 <amarachukwu.ogbu1993@gmail.com>
2021-06-23 10:55:49 +0000

    removed white space for betty style

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,16 +11,12 @@ int print_last_digit(int n)
 {
 	int last_digit = n % 10;
 
-{
-	if (last_digit < 10)
+	if (n < 0)
 
-	{
-	_putchar(-last_digit + '0');
-	return (-last_digit);
-	}
+	last_digit = last_digit * -1;
+
 	else
-	{
+
 	_putchar(last_digit + '0');
 	return (last_digit);
-	}
 }
