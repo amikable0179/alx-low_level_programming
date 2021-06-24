853068a845aa5e7bf02d54e969f99b2b6e099e7e
amikable0179 <amarachukwu.ogbu1993@gmail.com>
2021-06-24 10:41:31 +0000

    multiplication table  0 to 15

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,52 +1,49 @@
 #include "holberton.h"
-
 /**
-* print_times_table - prints the times table
-* @n: integer for which the times table will be printed
-*
-* Description: prints the times table
-*
-* Return: void
+* print_times_table  -prints tables
+*@n: print int
+* Return: nothing
 */
-
 void print_times_table(int n)
 {
-	int row, column, product;
-
-	if (n >= 0 && n < 15)
+	int a, b, c; /*Declaring statements*/
+	if (n < 16 && n > -1)
 	{
-	for (row = 0; row <= n; row++)
+	for (a = 0; a <= n; a++)/*First for*/
 	{
-	for (column = 0; column <= n; column++)
+	for (b = 0; b <= n; b++)/*Secondo for*/
 	{
-	product = row * column;
-
-	if (column == 0)
-	_putchar('0');
-	else if (product < 10)
+	c = a * b;/* multiplication a*b*/
+	if ((c / 10) == 0)/*If the first digit is 0*/
+	{
+	if  (b == 0)
+	{ _putchar ('0');
+	}
+	if (b != 0)/*print if b is different 0*/
+	{
+	_putchar (' ');
+	_putchar (' ');
+	_putchar ((c % 10) + '0');
+	}
+	} /*End the first IF*/
+	else if ((c / 10) <= 9 && ((c % 10) <= 9))/*print to 99*/
+	{
+	_putchar (' ');
+	_putchar ((c / 10) + '0');
+	_putchar ((c % 10) + '0');
+	} /*End ELSE IF*/
+	else/*print from 100*/
+	{
+	_putchar ((c / 100) + '0');
+	_putchar (((c / 10) % 10)  + '0');
+	_putchar ((c % 10) + '0');
+	} /*End ELSE*/
+	if (b < n) /*don't print the last (;)*/
 	{
 	_putchar(',');
-	_putchar(' ');
-	_putchar(' ');
-	_putchar(' ');
-	_putchar(product % 10 + '0');
+	_putchar (' ');
 	}
-	else if (product >= 10 && product < 100)
-	{
-	_putchar(',');
-	_putchar(' ');
-	_putchar(' ');
-	_putchar((product / 10) % 10 + '0');
-	_putchar(product % 10 + '0');
+	} /*End second FOR*/ _putchar ('\n');
+	} /*End first FOR*/
 	}
-	else if (product > 99 && product < 1000)
-	_putchar(',');
-	_putchar(' ');
-	_putchar(product / 100 + '0');
-	_putchar((product / 10) % 10 + '0');
-	_putchar(product % 10 + '0');
-	}
-	}
-	_putchar('\n');
-	}
-}
+} /*End int main*/
