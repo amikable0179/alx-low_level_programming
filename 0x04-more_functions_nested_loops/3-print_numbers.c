1e927498cbf2e7cbad8e2503b1fe7535f6675cfe
amikable0179 <amarachukwu.ogbu1993@gmail.com>
2021-06-25 14:48:44 +0000

    numbers 1 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+* print_numbers - Entry point
+*
+* Return: no return
+*
+*/
+
+void print_numbers(void)
+{
+	char x = 0;
+
+	for (x = '0'; x <= '9'; x++)
+
+	{
+	_putchar('x');
+	}
+	_putchar('\n');
+
+}
