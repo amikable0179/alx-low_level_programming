bbb8e495e8557759bc39fa24b07db6457f1f218e
amikable0179 <amarachukwu.ogbu1993@gmail.com>
2021-06-28 13:20:39 +0000

    prints a string followed by new line to stdout

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,6 +1,5 @@
 #include "holberton.h"
 /**
-*
 * _puts - prints a string in stdout
 * @str: string to print
 *
@@ -9,7 +8,7 @@
 void _puts(char *str)
 {
 	int i = 0;
-	
+
 	while (*(str + i))
 	{
 	_putchar(*(str + i));
