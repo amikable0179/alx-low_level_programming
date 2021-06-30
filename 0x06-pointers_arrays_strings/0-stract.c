1d01cfd00627583c4434be5bd9edebf99a1bfac4
amikable0179 <amarachukwu.ogbu1993@gmail.com>
2021-06-30 13:20:52 +0000

    header file, _putchar, main.c, and a function that concatenate pointers

---
+++ 0x06-pointers_arrays_strings/0-stract.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+* strcat - Concatenates the string pointed to by @src, including the terminating
+* null byte, to the end of the string pointed to by @dest.
+* @dest: A pointer to the string to be concantenated.
+* @src: the source string to be concantenated.
+*
+* return: A pointer to the destination string @dest.
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	_strcat(dest, src);
+}
---
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -0,0 +1,7 @@
+#ifndef HOLBERTON_H_
+#define HOLBERTON_H_
+
+int _putchar(char c)
+char *_strcat(char *dest, char *src);
+
+#endif
