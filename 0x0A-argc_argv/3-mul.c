5fb89439954cb8e3e9e12226ee24486e0202de07
amikable0179 <amarachukwu.ogbu1993@gmail.com>
2021-07-12 15:47:47 +0000

    argc argv multiplication and addition

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - entry point
+ *@argc: number of command line arguments
+ *@argv: array of pointers to argument strings
+ *
+ *Description: multiplies two numbers
+ *Return: 0 upon successful completion
+ */
+
+int main(int argc, char *argv[])
+{
+	int count = 1, mul;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		mul = atoi(argv[count]);
+		mul *= atoi(argv[count + 1]);
+		printf("%d\n", mul);
+		return (0);
+	}
+}
