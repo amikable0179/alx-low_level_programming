30b3d4b067ce6611169f2592c3186a9d23f97630
amikable0179 <amarachukwu.ogbu1993@gmail.com>
2021-07-20 08:52:22 +0000

    user creation newuser, free

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,13 +7,16 @@
  * @owner: the owner og the dog
  */
 
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
 
+typedef struct dog dog_t;
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
