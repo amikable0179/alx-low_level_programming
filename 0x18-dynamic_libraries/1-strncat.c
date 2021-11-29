7262415491395ce3c4dfcab97a9209e09088280f
amikable0179 <amarachukwu.ogbu1993@gmail.com>
2021-11-29 20:25:54 +0000

    all

---
+++ 0x18-dynamic_libraries/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* _strncat- Copy str to dest
+* @n: Number of bytes to use max
+* @dest: String to copied to
+* @src: String to be copied to dest
+* Return: Always 0.
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char *origin = dest;
+	int counter = 0;
+
+	while (*dest != '\0')
+		dest++;
+
+	while (counter < n && *src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		counter++;
+	}
+	*dest = '\0';
+	return (origin);
+}
